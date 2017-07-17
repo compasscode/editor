95f368a6e19a20793f24bde3bdc08fd714a3863e
alex <16batesa@gmail.com>
2017-07-17 21:19:25 +0100

    Add basic, yet working Codeareas and output iframe
    IT WORKS!

--- js/editor.js
+++ js/editor.js
@@ -1,13 +1,31 @@
 import 'babel-polyfill'
 import 'web-animations-js'
+
+import Element from './UI/Element'
 import Pane, { ResizablePane } from './UI/Pane'
+import Codearea from './UI/Codearea'
 
-// DOM is loaded; let's build the app!
-const editor = new Pane('editor')
-console.log(editor)
+const editor = new Pane('editor f--row')
+const leftPane = new Pane('pane--left f-g2 f--col')
+const rightPane = new Pane('pane--right f-g3')
 
-// Add the editor Pane to the body
-document.body.appendChild(editor.el)
+// Add the output iframe to the right pane
+let iframe = editor.outputFrame = new Element('iframe', 'iframe--output')
+iframe.el.setAttribute('sandbox', 'allow-modals allow-scripts allow-same-origin')
+iframe.el.src = 'about:blank'
+rightPane.add(iframe)
+
+// Add code panes to the left pane
+let langs = [ 'html', 'css', 'js' ]
+for (let lang of langs) {
+	let pane = new Pane(`pane--lang pane--lang-${lang} f-g1`)
+	let codearea = new Codearea(lang, iframe)
+
+	leftPane.add(pane.add(codearea))
+}
+
+// Add the editor to the body
+document.body.appendChild(editor.add(leftPane).add(rightPane).el)
 
 // Reveal the editor underneath the
 // loader, then remove the loader.
@@ -16,10 +34,10 @@ let anim = document.querySelector('.loader').animate([
   { transform: 'translateY(0)' },
 	{ transform: `translateY(100%)` },
 ], {
-	duration: 1000,
-	delay: 250,
+	duration: 500,
+	delay: 100,
 	fill: 'forwards',
-	easing: 'ease-in',
+	easing: 'ease-in-out',
 })
 
 anim.onfinish = () => document.querySelector('.loader').remove()
