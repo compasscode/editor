9cf67b710f8314275c1632dde2737d14c5b67acf
Josh Pullen <JoshPullen27@gmail.com>
2017-07-18 23:26:16 -0400

    Add navigation links, move project actions to toolbar

--- js/UI/Toolbar.js
+++ js/UI/Toolbar.js
@@ -5,20 +5,13 @@ export default class Toolbar extends Pane {
 	constructor(className) {
 		super(className)
 
-		let logo = new Element('div', 'toolbar-logo')
-		let titleInput = new Element('input', 'toolbar-titleInput')
-		let saveButton = new Element('button', 'toolbar-saveButton')
-		let shareButton = new Element('button', 'toolbar-shareButton')
-		let user = new Element('div', 'toolbar-user')
-		
+		let titleInput = new Element('input', `${className}-titleInput`)
+		titleInput.el.setAttribute('type', 'text')
+		titleInput.el.setAttribute('placeholder', 'Project title...')
+		this.add(titleInput)
+
+		let saveButton = new Element('button', `${className}-saveButton`)
 		saveButton.el.innerHTML = 'Save'
-		shareButton.el.innerHTML = 'Share'
-		titleInput.el.placeholder = 'Project title'
-		
-		this.add(logo)
-			.add(titleInput)
-			.add(saveButton)
-			.add(shareButton)
-			.add(user)
+		this.add(saveButton)
 	}
 }
